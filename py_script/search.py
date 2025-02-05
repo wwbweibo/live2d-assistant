9ac0d3d9dfe162637ea3721770109cfbee4e348c
wwbweibo <wwbweibo@outlook.com>
2025-02-06 00:04:17 +0800

    完善搜索增强

--- py_script/search.py
+++ py_script/search.py
@@ -2,56 +2,39 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from llm import OllamaClient
+from utils.selenium import WebDriverManager
+from concurrent.futures import ThreadPoolExecutor
 
-class DuckDuckGoSearchEngine():
+class SearchResultItem:
+    def __init__(self, title: str, preview: str, url: str):
+        self.title = title
+        self.preview = preview
+        self.url = url
+
+class BaseSearchEngine():
     def __init__(self, ollama_client: OllamaClient):
-        self.engine_name = 'DuckDuckGO'
         self.ollama_client = ollama_client
-        self.url = "https://duckduckgo.com/?t=h_&ia=web&q="
-
-    def search(self, query: str) -> str:
-        req = requests.get(self.url + query, headers={
-            "user-agent": "Moz/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-            "cache-control": "no-cache",
-            "pragma": "no-cache",
-            "priority": "u=0, i",
-            "referer": "https://duckduckgo.com/",
-            "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Microsoft Edge";v="132"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "Windows",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "priority": "u=0, i"
-        })
-        # 提取相应内容，获取到返回的网页内容
-        content = req.text
-        # 使用BeautifulSoup解析网页内容
-        soup = BeautifulSoup(content, 'html.parser')
-        # 提取网页内容中的标题和描述
-        result_list = soup.find('li')
-        formatted_result = []
-        for result in result_list:
-            # 提取标题和预览文本
-            title_html = result.find('h2')
-            title = title_html.text
-            preview = result.find('span').text
-            url = title_html.find('a')['href']
-            formatted_result.append(f"{title}\n{preview}\n{url}")
-        return formatted_result
 
     def find_top_bese_match_article(self, text: str, result_list: list) -> str:
         format_result_list_2_text = ""
         for idx, result in enumerate(result_list):
-            format_result_list_2_text += f"{idx + 1}: 标题：{result['title']}; 内容：{result['content']};\n"
+            format_result_list_2_text += f"{idx + 1}: 标题：{result.title}; 内容：{result.preview};\n"
         prompt = f"""接下来给出的是一段话是由搜索引擎返回的搜索结果，请根据搜索结果，返回最符合用户搜索意图的搜索结果的前5条记录，你无需给出搜索结果的内容，仅需要返回满足条件的搜索结果序号即可。
-        搜索结果：{format_result_list_2_text}
+        ---
+        以下是你应该遵循的示例：
+        搜索结果：
+        1: 标题：Python 单元测试; 内容：Python 单元测试是 Python 编程中的一种测试方法，用于测试代码的各个模块是否能够正常工作。;
+        2: 标题：Python 单元测试; 内容：Python 单元测试是 Python 编程中的一种测试方法，用于测试代码的各个模块是否能够正常工作。;
+        3: 标题：Python 单元测试; 内容：Python 单元测试是 Python 编程中的一种测试方法，用于测试代码的各个模块是否能够正常工作。;
+        4: 标题：Python 单元测试; 内容：Python 单元测试是 Python 编程中的一种测试方法，用于测试代码的各个模块是否能够正常工作。;
+        5: 标题：Python 单元测试; 内容：Python 单元测试是 Python 编程中的一种测试方法，用于测试代码的各个模块是否能够正常工作。;
+        用户的搜索意图：我想学习Python的单元测试，应该怎么学？
+        期望的返回结果：1,3
+        ---
+        以下是你需要根据用户提供的搜索意图，返回最符合用户搜索意图的搜索结果的前5条记录，你无需给出搜索结果的内容，仅需要返回满足条件的搜索结果序号，使用英文逗号分隔，前后不要有空格。回答的前后不要有任何内容。
+        搜索结果：
+        {format_result_list_2_text}
         用户搜索意图：{text}
-        最符合用户搜索意图的搜索结果：
         """
         req = {
             "model": "deepseek-r1:14b",
@@ -59,7 +42,6 @@ class DuckDuckGoSearchEngine():
             "stream": False
         }
         return self.ollama_client.generate(req)
-
 
     def before_search(self, text: str) -> str:
         prompt = f"""接下来的这段话是由用户提供的一段文本，其中包含了用户需要搜索的内容，请根据用户提供的内容，返回一组搜索引擎使用的查询关键词。你的回答只需要包含搜索查询语句，不要包含任何其他内容。
@@ -77,3 +59,80 @@ class DuckDuckGoSearchEngine():
             "stream": False
         }
         return self.ollama_client.generate(req)
+    
+    def read_search_result_detail(self, search_result: list[SearchResultItem], selected_result_idx: list[int]) -> str:
+        # 多个搜索选中的结果，并发请求获取详情
+        with ThreadPoolExecutor(max_workers=len(selected_result_idx)) as executor:
+            futures = [executor.submit(self.get_search_result_detail, search_result[idx]) for idx in selected_result_idx]
+            return [future.result() for future in futures]
+
+    def get_search_result_detail(self, search_result: SearchResultItem) -> str:
+        with WebDriverManager() as driver:
+            content = driver.get_page_content(search_result.url)
+            soup = BeautifulSoup(content, 'html.parser')
+            return soup.text
+        
+    # 一个抽象的搜索方法，需要子类实现
+    def search(self, query: str) -> list[SearchResultItem]:
+        pass
+
+    def searh_workflow(self, text: str) -> SearchResultItem:
+        query = self.before_search(text)
+        print("提取的查询语句：", query)
+        result_list = self.search(query)
+        print("搜索结果：", result_list)
+        top_result =  self.find_top_bese_match_article(text, result_list)
+        print("最符合用户搜索意图的搜索结果：", top_result)
+        selected_result_idx = [int(idx) for idx in top_result.split(',')]
+        selected_result_detail = self.read_search_result_detail(result_list, selected_result_idx)
+        print("选中的搜索结果详情：", selected_result_detail)
+        return selected_result_detail
+
+class DuckDuckGoSearchEngine(BaseSearchEngine):
+    def __init__(self, ollama_client: OllamaClient):
+        super().__init__(ollama_client)
+        self.engine_name = 'DuckDuckGO'
+        self.url = "https://duckduckgo.com/?t=h_&ia=web&q="
+
+    def search(self, query: str) -> list[SearchResultItem]:
+        with WebDriverManager() as driver:
+            if driver:
+                driver.get(self.url + query)
+                content = driver.page_source
+                soup = BeautifulSoup(content, 'html.parser')
+                # 提取网页内容中的标题和描述
+                result_list = soup.find('li')
+                formatted_result = []
+                for result in result_list:
+                # 提取标题和预览文本
+                    title_html = result.find('h2')
+                    title = title_html.text
+                    preview = result.find('span').text
+                    url = title_html.find('a')['href']
+                    formatted_result.append(SearchResultItem(title, preview, url))
+        return formatted_result
+
+
+class BingSearchEngine(BaseSearchEngine):
+    def __init__(self, ollama_client: OllamaClient):
+        super().__init__(ollama_client)
+        self.engine_name = 'Bing'
+        self.url = "https://www.bing.com/search?q="
+
+    def search(self, query: str) -> str:
+        with WebDriverManager() as driver:
+            content = driver.get_page_content(self.url + query)
+            soup = BeautifulSoup(content, 'html.parser')
+            # 获取 <li class="b_algo">
+            li_list = soup.find_all('li', class_='b_algo')
+            result_list = []
+            for li in li_list:
+                title = li.find('h2')
+                url = title.find('a')['href']
+                title_text = title.text
+                preview = li.find('div', class_='b_caption')
+                preview_text = preview.text
+                result_list.append(SearchResultItem(title_text, preview_text, url))
+            return result_list
+
+
